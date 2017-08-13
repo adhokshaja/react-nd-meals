54050198ba165d8facc66ce88c645345b04ae26c
adhokshaja <adok.achar@gmail.com>
2017-08-13 18:23:16 -0400

    feat: Added actions

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+export const ADD_RECIPE = 'ADD_RECIPE'
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
+
+//Use constatnts because strings could have spelling mistakes.
+
+//Action creators
+
+export function addRecipe({ day, recipe, meal }) {
+    return {
+        type: ADD_RECIPE,
+        day,
+        meal,
+        recipe
+    }
+}
+
+export function removeFromCalendar({ day, meal }) {
+    return {
+        type: REMOVE_FROM_CALENDAR,
+        day,
+        meal
+    }
+}
